2678b906cba7db16de6ca14ac4744f3bbb68f1af
willcagas <williamgabriel.cagas@gmail.com>
2024-05-21 13:27:05 -0400

    Final Touches

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,41 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import GitHubButton from 'react-github-button';
+
 import logo from './../assets/images/logo.png';
+import music from './../assets/sounds/PVZtheme.mp3';
 
 import './../assets/styles/Navbar.css';
+import { Howl } from 'howler';
 
 const Header = () => {
+    const [musicPlaying, setMusicPlaying] = useState(false);
+
+    const playMusic = () => {
+        if (!musicPlaying) {
+        const bgMusic = new Howl({
+            src: [music],
+            loop: true,
+            volume: 0.5,
+        });
+
+        bgMusic.play();
+        setMusicPlaying(true);
+        }
+    }
+
     return (
         <header>
-            <img className="site-logo" height={55} src={logo} alt=''/>
+            <button onClick={playMusic}>
+                <img className="site-logo" height={55} src={logo} alt=''/>
+            </button>
             
             <nav>
                 <Link to="/" >Home</Link>
                 <Link to="/post-listing">Post</Link>
+
             </nav>
         </header>
     );
